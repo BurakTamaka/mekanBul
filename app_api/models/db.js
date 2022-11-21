e217b9193b1d815d55eaf610598447502e3bf7b5
BurakTamaka <buraktamaka00@gmail.com>
2022-11-22 00:07:21 +0300

    odev5

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,33 +1,29 @@
 var mongoose=require("mongoose");
-require("./mekansema");
-//var dbURI = 'mongodb://localhost/mekanbul'; 
-var dbURI="mongodb+srv://BurakTamaka:1234@mekanbul.7mayngr.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(dbURI);
+var dbURI="mongodb+srv://burakTamaka:1234@mekanbul.7wuzndc.mongodb.net/?retryWrites=true&w=majority";
+//var dbURI="mongodb://localhost/mekanbul"
 
+mongoose.connect(dbURI);
 function kapat(msg,callback){
     mongoose.connection.close(function(){
         console.log(msg);
         callback();
     });
+
 }
-
-process.on("SIGINT",function(){
-    kapat("Uygulama kapatıldı",function(){
-        process.exit(0);
+process.on("SIGNIT",function(){
+    kapat("uygulama kapatildi",function(){
+    process.exit(0);
     });
-})
-
-
+});
 mongoose.connection.on("connected",function(){
-    console.log(dbURI+"adresindeki veritabanına bağandı.");
-})
-
-
+console.log(dbURI +"Adresindeki veritabanına bağlandı");
+});
 mongoose.connection.on("disconnected",function(){
-    console.log("Bağlantı koptu.");
-})
+    console.log("baglanti koptu");
+});
+mongoose.connection.on("error",function(){
+        console.log("bağlantı hatası");
+});
+        
 
-
-mongoose.connection.on("connected",function(){
-    console.log("Bağlantı hatası.");
-})
+require("./mekansema");
--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,7 +1,5 @@
 var express = require("express");
 var router = express.Router();
-
-// slash gördüğün zaman function çalışır
 const anaSayfa = function (req, res, next) {
   res.render("anasayfa", {
     "baslik": "Anasayfa",
@@ -15,19 +13,15 @@ const anaSayfa = function (req, res, next) {
         "adres":"Sdü Batı Kampüsü",
         "puan":"4",
         "mesafe":"2km",
-        "imkanlar":["Kahve","Çay","Pasta"]
+        "imkanlar":["Kahve","Çay","çikolata"]
       },
       {
         "ad":"Gloria",
         "adres":"Sdü Doğu Kampüsü",
         "puan":"2",
         "mesafe":"5km",
-        "imkanlar":["Kahve","Çay","Pasta"]
-      }
-
-
-
-    ]
+        "imkanlar":["Gazoz","Tavuk döner","Poğaca"]
+      }]
   });
 };
 
@@ -51,39 +45,30 @@ const mekanBilgisi = function (req, res, next) {
           "acilis":"10:00",
           "kapanis":"22:00",
           "kapali": false
-        }
-      ],
-      "imkanlar":["Kahve","Çay","Pasta"],
+        }],
+      "imkanlar":["Kahve","Çay","çikolata"],
       "koordinatlar":{
         "enlem":"37.7",
         "boylam":"30.5"
       },
       "yorumlar":[
         {
-          "yorumYapan":"Sinan",
-          "puan":"3",
-          "tarih":"20 Ekim 2022",
-          "yorumMetni":"idare eder"
+          "yorumYapan":"Emre",
+          "puan":"8",
+          "tarih":"28 Haziran 2000",
+          "yorumMetni":"Çok güzel mekan enişte."
         },
         {
-          "yorumYapan":"burak",
+          "yorumYapan":"Furkan",
           "puan":"2",
-          "tarih":"20 Ekim 2022",
-          "yorumMetni":"hiç begenmedim"
-        }       
-      ]
-
+          "tarih":"13 Temmuz 1996",
+          "yorumMetni":"Kötü mekan kardeşim."
+        }]
    }
-
-
-
-
-
 });
 };
 
 const yorumEkle = function (req, res, next) {
   res.render("yorumekle", { title: "Yorum Sayfası" });
 };
-
 module.exports = { anaSayfa, mekanBilgisi, yorumEkle };
