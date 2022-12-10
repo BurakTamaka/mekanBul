3924fa22cf2186047028152453b28aa103b6d8e5
BurakTamaka <buraktamaka00@gmail.com>
2022-12-10 17:02:58 +0300

    odev8

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+var session=require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,11 +9,15 @@ var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var apiRouter = require('./app_api/routes/index');
 
-
 var app = express();
 
+app.use(session({
+  secret:'gizli',
+  cookie:{maxAge:1000*60*60*24},
+  resave:true,
+  saveUninitialized:true
+}));
 
-// view engine setup
 app.set('views', path.join(__dirname,'app_server', 'views'));
 app.set('view engine', 'pug');
 
